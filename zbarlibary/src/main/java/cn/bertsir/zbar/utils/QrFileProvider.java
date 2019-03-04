16dbda816ddb624cf846fd3b045ebb2bbf4e2778
Bert <bertsir@163.com>
2019-03-04 17:14:48 +0800

    解决FileProvider冲突

---
+++ zbarlibary/src/main/java/cn/bertsir/zbar/utils/QrFileProvider.java
@@ -0,0 +1,10 @@
+package cn.bertsir.zbar.utils;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Bert on 2019/3/4.
+ * Mail: bertsir@163.com
+ */
+public class QrFileProvider extends FileProvider {
+}
